0a5914579a9904a831a1d5a83147b5448ac8da81
saksham <saksham3939@gmail.com>
2020-12-23 21:49:49 +0530

    added a new unique code generation function to helpers.py

--- helpers.py
+++ helpers.py
@@ -1,6 +1,8 @@
 from flask import redirect, render_template, request, session
 from functools import wraps
-
+from cs50 import SQL 
+import random
+import string
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -8,3 +10,26 @@ def login_required(f):
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
+
+def makeRandomString(l):
+    random_string = ''.join(random.choices(string.ascii_uppercase +
+                             string.digits, k = l))
+    return random_string
+
+#takes the database and returns a unique code
+def create_code(database):
+    already_taken = True
+    code = ""
+    while already_taken:
+        code = makeRandomString(8)
+        rows = database.execute("SELECT code FROM classes WHERE code = :code", code = code)
+        if not len(rows) > 0:
+            already_taken = False
+    return code
+    
+
+# db = SQL("sqlite:///database.db")
+# someCode = "ABCDEFGH"
+
+# code = create_code(db)
+# print(code)
